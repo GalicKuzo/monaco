f31274267d5cbd7a580616378ba0121f4e26be9d
GalicKuzo <fernando.meza.perales@gmail.com>
2025-09-26 16:46:26 -0500

    main

--- src/main/java/com/monaco/monaco/MonacoApplication.java
+++ src/main/java/com/monaco/monaco/MonacoApplication.java
@@ -2,12 +2,18 @@ package com.monaco.monaco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MonacoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MonacoApplication.class, args);
+		SpringApplication app = new SpringApplication(MonacoApplication.class);
+        ConfigurableApplicationContext ctx = app.run(args);
+        // Obtener el puerto que Spring Boot está usando
+        String port = ctx.getEnvironment().getProperty("server.port");
+        System.out.println(">>> Aplicación iniciada, puerto: " + port);
 	}
 
 }
